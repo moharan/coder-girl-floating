429ed1a603728f5d7bea825bf535f395d66b7f27
RangiBecerra <rangibecerra@gmail.com>
2017-05-21 00:57:28 -0400

    css terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-alert("existe");
+//alert("existe");
+function validar() {
+	var nombre = document.getElementById("name").value;
+	console.log(nombre);
+    var email = document.getElementById("email").value;
+    console.log(email);
+    var asunto = document.getElementById("asunto").value;
+    console.log(asunto);
+    var mensaje = document.getElementById("mensaje").value;
+    console.log(mensaje);
+}
